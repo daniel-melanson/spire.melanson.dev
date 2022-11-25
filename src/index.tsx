73e1d0ad16352c8c5824a59533daa9cd21d028ca
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-25 17:30:49 -0500

    feat: umass theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
@@ -12,5 +12,5 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );

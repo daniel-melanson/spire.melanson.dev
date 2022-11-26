44cbff07ae4fe0f7302d9093f5483ee4233047e4
daniel-melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-11-26 14:41:23 -0500

    feat: stub academic group page

--- src/routes/AcademicGroup.tsx
+++ src/routes/AcademicGroup.tsx
@@ -1,7 +1,25 @@
-import { useLoaderData } from "react-router-dom";
+import { Card, Container } from "react-bootstrap";
+import { Link, useLoaderData } from "react-router-dom";
+import { AcademicGroup as APIAcademicGroup } from "../api";
 
 export default function AcademicGroup() {
-  const group = useLoaderData();
+  const group = useLoaderData() as APIAcademicGroup;
 
-  return <div>{JSON.stringify(group, undefined, 2)}</div>;
+  return (
+    <Container>
+      <h1>{group.title}</h1>
+      <h2>Subjects:</h2>
+      <div className="d-flex align-content-start flex-wrap p-2">
+        {group.subjects.map(subject => (
+          <Card className="m-1" key={subject.id}>
+            <Card.Body>
+              <Card.Title>{subject.id}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{subject.title}</Card.Subtitle>
+              <Link to={`/courses/${subject.id}`}>More Info</Link>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
 }
